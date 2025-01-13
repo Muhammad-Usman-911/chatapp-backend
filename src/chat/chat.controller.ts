13cbc94421f90ca23e66a65b6a5b66c1e55df9fd
Mo.Usman <musman38302@gmail.com>
2025-01-13 10:47:12 +0500

    `Fetching User Messages`

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -2,12 +2,16 @@ import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller('chat')
+@Controller('/chat')
 export class ChatController {
   constructor(private chatService: ChatService) {}
-
-  @Get('messages/:otherUserId')
-  @UseGuards(AuthGuard('jwt'))
+  @Get('/')
+  async checkMessage(){
+    return {
+      message:"working fine.",
+    }
+  }
+  @Get('/messages/:userId/:otherUserId')
   async getMessages(@Param('otherUserId') otherUserId: number, @Param('userId') userId: number) {
     try {
       const messages = await this.chatService.getMessages(userId, otherUserId);
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { PrismaService } from 'src/prisma.service';
+import { ChatController } from './chat.controller';
 
 @Module({
   providers: [ChatGateway, ChatService,PrismaService],
+  controllers:[ChatController]
 })
 export class ChatModule {}
